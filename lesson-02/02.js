192eb0f3ec020775da4dcb3960587892d6d08a75
Julya Shabala <shabala.julya@gmail.com>
2024-11-20 09:31:34 +0300

    complete lesson-2 task-2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,9 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random() * 7) // your code
+let dice2 = Math.floor(Math.random() * 7) // your code
+let isWinningDouble = false // your code
 
 /*
 console.log('Первый бросок: ' + dice1)
@@ -20,3 +20,17 @@ if (isWinningDouble) {
   console.log('Не выигрышный дубль.')
 }
 */
+console.log('Первый бросок: ' + dice1)
+console.log('Второй бросок: ' + dice2)
+
+if(dice1 === dice2 && dice1 >3 && dice1 <= 6){
+  isWinningDouble = true
+} else {
+  isWinningDouble = false
+}
+
+if (isWinningDouble) {
+  console.log('Выигрышный дубль!')
+} else {
+  console.log('Не выигрышный дубль.')
+}

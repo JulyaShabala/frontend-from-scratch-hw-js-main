e54ab33b41b731da8958156ac126a82e0dcebf04
Julya Shabala <shabala.julya@gmail.com>
2025-01-17 16:43:53 +0300

    lesson-6 task-1

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -17,3 +17,23 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+
+const prevButton = document.getElementById('prev-button')
+const nextButton = document.getElementById('next-button')
+const webTechImage = document.getElementById('web-tech-image')
+let index = 0
+const arrLength = WEB_TECH_IMAGES.length
+
+nextButton.addEventListener('click', function(){
+  changeImage(index + 1)
+})
+
+prevButton.addEventListener('click', function(){
+  changeImage(index - 1)
+})
+
+function changeImage(i){
+  index = (i + arrLength) % arrLength
+  webTechImage.src = WEB_TECH_IMAGES[index]
+}

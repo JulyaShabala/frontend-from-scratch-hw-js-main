1ab447d7d5111cee0ddb7b3feec7b630b3102c96
Julya Shabala <shabala.julya@gmail.com>
2025-01-20 17:22:17 +0300

    lesson-7 task-2

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -4,10 +4,11 @@
  */
 
 function isNumeric(str) {
+  return !isNaN(str)
   // your code
 }
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
+//console.log(isNumeric("123")) // Ожидаемый результат: true
 // console.log(isNumeric("12.3")) // Ожидаемый результат: true
 // console.log(isNumeric("123abc")) // Ожидаемый результат: false
 // console.log(isNumeric("abc")) // Ожидаемый результат: false
